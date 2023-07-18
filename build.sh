2b72403555ae277aad40148efaacfa3fecf4161a
Eugene Medvedev <rn3aoh.g@gmail.com>
2023-07-18 18:21:37 +0300

    Release form as zip file too.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -f build/ffwn.zip
+7z a build/ffwn.zip form/*
 env GOOS=linux GOARCH=amd64 go build -o build/ffwn-checkout_linux -ldflags '-s -w'
 env GOOS=windows GOARCH=amd64 go build -o build/ffwn-checkout.exe -ldflags '-s -w'
 env GOOS=darwin GOARCH=amd64 go build -o build/ffwn-checkout_osx -ldflags '-s -w'
